d15d424afc27c4880a798dffb6cf677e143a75dc
Hritik777 <52399623+Hritik777@users.noreply.github.com>
2021-07-13 14:38:15 +0545

    new insurance test added

--- tests/taxpayer.test.js
+++ tests/taxpayer.test.js
@@ -3,20 +3,39 @@ const request = require('supertest');
 // we also need our app for the correct routes!
 const app = require('../index');
  
-describe('POST taxpayer info', () => {
+//describe('POST taxpayer info', () => {
+  //jest.setTimeout(30000);
+  //test('It should post taxpayer details and return tax details', async () => {
+   // const stateObj = {
+    //  bluebook_number: '5',
+     // vehicle_number: '5555',
+      //policy_number: '6556',
+     // engine_cc: 350,
+     // bluebook_file_path: '/bluebook_file_path',
+      //citizenship_file_path: '/citizenship_file_path',
+     // policy_file_path: '/policy_file_path',
+   // };
+    //jest.setTimeout(50000);
+    //const response = await request(app).post('/api/taxpayer/').send(stateObj);
+    //jest.setTimeout(100000);
+    //expect(response.body.success).toEqual(true);
+  //});
+//});
+
+describe('POST insurance info', () => {
   jest.setTimeout(30000);
   test('It should post taxpayer details and return tax details', async () => {
     const stateObj = {
       bluebook_number: '5',
       vehicle_number: '5555',
-      policy_number: '6556',
+      insurance_company: "ABC",
       engine_cc: 350,
       bluebook_file_path: '/bluebook_file_path',
       citizenship_file_path: '/citizenship_file_path',
-      policy_file_path: '/policy_file_path',
+     
     };
     jest.setTimeout(50000);
-    const response = await request(app).post('/api/taxpayer/').send(stateObj);
+    const response = await request(app).post('/api/insurance/').send(stateObj);
     jest.setTimeout(100000);
     expect(response.body.success).toEqual(true);
   });
