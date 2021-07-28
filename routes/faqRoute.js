94954646042a3696eeb3f99272bcfdb624c07813
Anil Gajurel <anil037shivaa@gmail.com>
2021-07-28 16:19:52 +0545

    added route to fetch frequently asked questions and answers

--- routes/faqRoute.js
+++ routes/faqRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const FAQs = asyncHandler(async (req, res) => {
 
-   
+    if (req.method === "POST") {
         const {
             question,
             answer
@@ -21,9 +21,21 @@ const FAQs = asyncHandler(async (req, res) => {
         const faqsInserted = await faqs.save();
         res.status(200).
             send({ success: true, faqsInserted });
-
+    }
+    else if (req.method == "GET") {
+        const getFAQs = await FAQ.find();
+        try {
+            res.status(200).send({ success: true, getFAQs: getFAQs });
+        } catch (err) {
+            console.log(`Error occured in /api/tax-faq/ get request: ${err}`);
+            res.status(404).send({ success: false, message: err });
+        }
+    }
+    else {
+        res.status(404).send({ success: false, message: req.method + " is not allowed" });
+    }
 })
 
-router.route('/').post(FAQs)
+router.route('/').post(FAQs).get(FAQs);
 
 module.exports = router;
