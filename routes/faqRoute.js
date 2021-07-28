b77c964f4d89af7116b639114997d66b7f18fe59
Anil Gajurel <anil037shivaa@gmail.com>
2021-07-28 16:11:48 +0545

    created faqRoute to add frequently asked questions

---
+++ routes/faqRoute.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const FAQ = require('../models/faqModel');
+
+const router = express.Router();
+
+const FAQs = asyncHandler(async (req, res) => {
+
+   
+        const {
+            question,
+            answer
+        } = req.body;
+
+        const faqs = new FAQ({
+            question: question,
+            answer: answer,
+        });
+
+        const faqsInserted = await faqs.save();
+        res.status(200).
+            send({ success: true, faqsInserted });
+   
+})
+
+router.route('/').post(FAQs)
+
+module.exports = router;
