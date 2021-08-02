83c209636d202ab57a6d81bc853ecfafed1190b3
Rajkumar Layo Magar <raz.magar251@gmail.com>
2021-08-02 13:41:12 +0545

    Added insurance agent route

--- routes/insuranceAgentsRoute.js
+++ routes/insuranceAgentsRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
+ 
 const insuranceagents = require('../models/insuranceAgentsModel');
-
+ 
 const router = express.Router();
-
+ 
 const allInsuranceAgents = asyncHandler(async (req, res) => {
   const allInsuranceAgents = await insuranceagents.find();
-
+ 
   try {
     res
       .status(200)
@@ -17,7 +17,39 @@ const allInsuranceAgents = asyncHandler(async (req, res) => {
     res.status(404).send({ success: false, message: err });
   }
 });
-
-router.route('/').get(allInsuranceAgents);
-
+ 
+const addInsuranceAgents = asyncHandler(async (req, res) => {
+  const {
+    address,
+    contact,
+    email,
+    insurance_company,
+    license_number,
+    vat_number,
+    vat_file_path,
+    license_file_path,
+  } = req.body;
+ 
+  const insuranceagent = new insuranceagents({
+    address:address,
+    contact:contact,
+    email:email,
+    insurance_company: insurance_company,
+    license_number: license_number,
+    vat_number:vat_number,
+    docs: [{ vat_file_path, license_file_path }],
+  });
+ 
+  const InsertedInsuranceAgent = await insuranceagent.save();
+ 
+  try {
+    res.status(200).send({ success: true, InsertedInsuranceAgent });
+  } catch (error) {
+    res.status(503).send({ success: false, message:"Service Unavailable"});
+  }
+ 
+});
+ 
+router.route('/').get(allInsuranceAgents).post(addInsuranceAgents);
+ 
 module.exports = router;
