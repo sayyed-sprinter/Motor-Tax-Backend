2606a936423043e15a32ef16e2ec038b52487ee3
sayyed-sprinter <sprintersagile@gmail.com>
2021-08-02 15:43:57 +0545

    updated req.body name to match with frontend

--- routes/insuranceAgentsRoute.js
+++ routes/insuranceAgentsRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
- 
+
 const insuranceagents = require('../models/insuranceAgentsModel');
- 
+
 const router = express.Router();
- 
+
 const allInsuranceAgents = asyncHandler(async (req, res) => {
   const allInsuranceAgents = await insuranceagents.find();
- 
+
   try {
     res
       .status(200)
@@ -17,39 +17,38 @@ const allInsuranceAgents = asyncHandler(async (req, res) => {
     res.status(404).send({ success: false, message: err });
   }
 });
- 
+
 const addInsuranceAgents = asyncHandler(async (req, res) => {
   const {
     address,
     contact,
     email,
-    insurance_company,
-    license_number,
-    vat_number,
+    insuranceCompany,
+    licenseNumber,
+    vatNumber,
     vat_file_path,
     license_file_path,
   } = req.body;
- 
+
   const insuranceagent = new insuranceagents({
-    address:address,
-    contact:contact,
-    email:email,
-    insurance_company: insurance_company,
-    license_number: license_number,
-    vat_number:vat_number,
+    address: address,
+    contact: contact,
+    email: email,
+    insurance_company: insuranceCompany,
+    license_number: licenseNumber,
+    vat_number: vatNumber,
     docs: [{ vat_file_path, license_file_path }],
   });
- 
+
   const InsertedInsuranceAgent = await insuranceagent.save();
- 
+
   try {
     res.status(200).send({ success: true, InsertedInsuranceAgent });
   } catch (error) {
-    res.status(503).send({ success: false, message:"Service Unavailable"});
+    res.status(503).send({ success: false, message: 'Service Unavailable' });
   }
- 
 });
- 
+
 router.route('/').get(allInsuranceAgents).post(addInsuranceAgents);
- 
+
 module.exports = router;
