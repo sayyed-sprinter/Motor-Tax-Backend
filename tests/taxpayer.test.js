92312157189eec3e6c5b4e5c76b9580ccc36ac34
Susan Dangol <susandangol747@gmail.com>
2021-08-03 21:24:36 +0545

    tdd tesing to get insurance agent document

--- tests/taxpayer.test.js
+++ tests/taxpayer.test.js
@@ -119,3 +119,14 @@ describe('GET latest insurance agent information', () => {
     expect(response.body.success).toEqual(true);
   });
 });
+
+
+describe('GET insurance agent documents', () => {
+  jest.setTimeout(30000);
+  test('It should fetch insurance agent documents', async () => {
+    jest.setTimeout(50000);
+    const response = await request(app).get('/api/insurance-agents');
+    jest.setTimeout(100000);
+    expect(response.body.success).toEqual(true);
+  });
+});
